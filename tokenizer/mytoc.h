6a072a4a2ab8ef1382ce8c56b077729f9f300688
Alex5Bal <abalderrama5@outlook.com>
2017-09-10 12:51:59 -0600

    Add files via upload

---
+++ tokenizer/mytoc.h
@@ -0,0 +1,8 @@
+#ifndef mytoc_included
+#define mytoc_included
+
+char** Mytoc(char* stringIn, char delim);
+int tokenCounter(char* stringIn);
+int* characterCounter(char* stringIn, int numTokens);
+
+#endif
---
+++ tokenizer/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mytoc.h"
+
+int main()
+{
+	char string[500];
+	int i, j;
+
+	do
+	{
+		printf("$ ");
+		//write(0, '$', 1);
+		fgets(string, 500, stdin);
+
+		char** tokenVec = Mytoc(string, ' ');
+		int tokenCount = tokenCounter(string);
+		int* numCharacters = characterCounter(string, tokenCount);
+
+		for (i = 0; i < tokenCount; i++)
+		{
+			printf("[%d] : ", i);
+			for (j = 0; j < numCharacters[i]; j++)
+			{
+				printf("%c", tokenVec[i][j]);
+			}
+			printf("\n");
+			}
+
+		free(tokenVec);
+	} while(string[0] != 'X');
+
+	return 0;
+}
